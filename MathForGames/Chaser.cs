20b602aa15f8af269079d4a1bd9759fb93662129
Katelyn <katelynwest225@gmail.com>
2021-10-25 16:03:15 -0500

    Gave player ability to run and edited how game tracked enemies

--- MathForGames/Chaser.cs
+++ MathForGames/Chaser.cs
@@ -77,6 +77,7 @@ namespace MathForGames
         {
             if (actor is Bullet)
             {
+                Engine.ActorDeath(this);
                 RemoveActor();
                 actor.RemoveActor();
             }
